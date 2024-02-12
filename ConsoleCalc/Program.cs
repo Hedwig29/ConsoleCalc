aa114014992524440633309ba47c6b1d2abe764d
Hedwig29 <156532418+Hedwig29@users.noreply.github.com>
2024-02-12 17:09:42 -0600

    1st version

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using static System.Collections.Specialized.BitVector32;
 
 class Calculator
 {
@@ -16,16 +17,74 @@ class Calculator
             Console.WriteLine("5. Выход");
 
             Console.Write("Выберите операцию (1/2/3/4/5): ");
-            
-            // Код пишем тут
-            // Не забываем про break;
-            // Не забываем про деление на ноль
-            // Не забываем про некорректный ввод (например, буквы)
+            string action = Console.ReadLine();
+            int.TryParse(action, out int value);
 
-            //Console.Write("Введите первое число: ");
+            double number1, number2, total;
 
-            //Console.Write("Введите второе число: ");
+            Console.WriteLine("Введите первое число");
+            number1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            number2 = double.Parse(Console.ReadLine());
 
+            switch (action)
+            {
+                case "1":
+                    total = number1 + number2;
+                    Console.WriteLine("Сумма " + number1 + " и " + number2 + " равна " + total + ".");
+                    Console.WriteLine("Для выхода нажмите любую клавишу...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                case "2":
+                    total = number1 - number2;
+                    Console.WriteLine("Разность " + number1 + " и " + number2 + " равна " + total + ".");
+                    Console.WriteLine("Для выхода нажмите любую клавишу...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                case "3":
+                    total = number1 * number2;
+                    Console.WriteLine("Умножение " + number1 + " на " + number2 + " равно " + total + ".");
+                    Console.WriteLine("Для выхода нажмите любую клавишу...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                case "4":
+                    total = number1 / number2;
+                    Console.WriteLine("Деление " + number1 + " и " + number2 + " равно " + total + ".");
+                    Console.WriteLine("Для выхода нажмите любую клавишу...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    if (number1 == 0)
+                    {
+                        Console.WriteLine(0);
+                    }
+                    if (number2 == 0)
+                    {
+                        Console.WriteLine(0);
+                    }
+                    break;
+                case "5":
+                    Console.Clear();
+                    break;
+
+                default:
+                    Console.WriteLine("Неизвестная команда,повторите попытку");
+                    Console.WriteLine("Для выхода нажмите любую клавишу...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                    // Код пишем тут
+                    // Не забываем про break;
+                    // Не забываем про деление на ноль
+                    // Не забываем про некорректный ввод (например, буквы)
+
+                    //Console.Write("Введите первое число: ");
+
+                    //Console.Write("Введите второе число: ");
+
+            }
         }
     }
 }
